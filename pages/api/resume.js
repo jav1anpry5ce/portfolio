02b9b71aad4d2b8f856cee8223cfea865a404d76
Javaughn <javanpryce1@gmail.com>
2022-02-26 07:13:19 -0500

    commit

--- pages/api/resume.js
+++ pages/api/resume.js
@@ -1,15 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-const fs = require("fs");
-const file = fs.createReadStream("/public/Javaughn_Pryce_Resume.pdf");
-const stat = fs.statSync("/public/Javaughn_Pryce_Resume.pdf");
+// const fs = require("fs");
+// const file = fs.createReadStream("/public/Javaughn_Pryce_Resume.pdf");
+// const stat = fs.statSync("/public/Javaughn_Pryce_Resume.pdf");
+import stream from "stream";
+import { promisify } from "util";
+import fetch from "node-fetch";
 
-export default function handler(req, res) {
-  res.setHeader("Content-Length", stat.size);
+const pipeline = promisify(stream.pipeline);
+const url = "https://javaughnpryce.live:5000/Javaughn_Pryce_Resume.pdf";
+
+export default async function handler(req, res) {
+  const response = await fetch(url); // replace this with your API call & options
+  if (!response.ok)
+    throw new Error(`unexpected response ${response.statusText}`);
+  
   res.setHeader("Content-Type", "application/pdf");
-  res.setHeader(
-    "Content-Disposition",
-    "attachment; filename=Javaughn_Pryce_Resume.pdf"
-  );
-  file.pipe(res);
+  res.setHeader("Content-Disposition", "attachment; filename=dummy.pdf");
+  await pipeline(response.body, res);
+
+  // res.setHeader("Content-Length", stat.size);
+  // res.setHeader("Content-Type", "application/pdf");
+  // res.setHeader(
+  //   "Content-Disposition",
+  //   "attachment; filename=Javaughn_Pryce_Resume.pdf"
+  // );
+  // file.pipe(res);
 }

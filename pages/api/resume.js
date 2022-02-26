fc6e388d602bc2d2a98c578cce058d9ae48be3bc
Javaughn <javanpryce1@gmail.com>
2022-02-26 00:49:03 -0500

    commit

--- pages/api/resume.js
+++ pages/api/resume.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const file = fs.createReadStream("././public/Javaughn_Pryce_Resume.pdf");
 const stat = fs.statSync("././public/Javaughn_Pryce_Resume.pdf");
 
-export default async function handler(req, res) {
+export default function handler(req, res) {
   res.setHeader("Content-Length", stat.size);
   res.setHeader("Content-Type", "application/pdf");
   res.setHeader(

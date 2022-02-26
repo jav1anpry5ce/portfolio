017c8a9f88ce045679e062bb17d341bb484aa347
Javaughn <javanpryce1@gmail.com>
2022-02-26 00:46:46 -0500

    commit

--- pages/api/resume.js
+++ pages/api/resume.js
@@ -1,8 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 const fs = require("fs");
-const file = fs.createReadStream("./public/Javaughn_Pryce_Resume.pdf");
-const stat = fs.statSync("./public/Javaughn_Pryce_Resume.pdf");
+const file = fs.createReadStream("././public/Javaughn_Pryce_Resume.pdf");
+const stat = fs.statSync("././public/Javaughn_Pryce_Resume.pdf");
 
 export default async function handler(req, res) {
   res.setHeader("Content-Length", stat.size);

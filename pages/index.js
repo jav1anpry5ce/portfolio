a657e070c63560dcaef717063d5a7e12d44063fe
Javaughn <javanpryce1@gmail.com>
2022-02-26 06:45:58 -0500

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import { Main, FindMe, MailMe, AboutMe, Work, Contact } from "../components";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="relative mx-auto my-2 w-full max-w-7xl flex-1 overflow-x-hidden">
+      <Head>
+        <title>Portfolio - Javaughn Pryce</title>
+      </Head>
       <MailMe />
       <FindMe />
       <Main />

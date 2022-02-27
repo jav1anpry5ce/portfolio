a478389151801a6123b3298b57d329b685905fc5
Javaughn <javanpryce1@gmail.com>
2022-02-27 12:34:04 -0500

    commit

--- components/Main.jsx
+++ components/Main.jsx
@@ -8,13 +8,15 @@ export default function Main() {
     <motion.div
       whileInView={{ opacity: [0, 1], x: [-300, 0] }}
       viewport={{ once: true }}
-      className="m-auto mb-20 flex h-[calc(100vh-71px)] max-w-4xl flex-1 flex-col justify-center px-6 sm:px-2"
+      className="mx-auto mb-20 flex min-h-[calc(100vh-71px)] max-w-4xl flex-1 flex-col justify-center px-4 sm:px-2"
       ref={mainRef}
     >
       <div className="space-y-5">
         <p className="text-lg text-secondary">Hi, my name is</p>
-        <h1 className="text-7xl font-bold text-gray-50">Javaughn Pryce.</h1>
-        <h2 className="text-6xl font-semibold text-gray-400">
+        <h1 className="text-6xl font-bold text-gray-50 md:text-7xl">
+          Javaughn Pryce.
+        </h1>
+        <h2 className="text-5xl font-semibold text-gray-400 md:text-6xl">
           I build full stack applications.
         </h2>
         <p className="max-w-lg text-xl text-gray-300">
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="relative mx-auto my-2 w-full max-w-7xl flex-1 overflow-x-hidden">
+    <div className="mx-auto my-2 w-full max-w-7xl flex-1 overflow-x-hidden">
       <Head>
         <title>Portfolio - Javaughn Pryce</title>
       </Head>

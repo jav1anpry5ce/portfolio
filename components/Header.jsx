3223835d2ff0f3080b0c1ef73ad21262690aeff8
Javaughn <javanpryce1@gmail.com>
2022-02-28 13:05:12 -0500

    commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,8 @@
-import React, { Fragment, useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 import { SimpleContext } from "../utils/simpleContext";
 import { FaBars } from "react-icons/fa";
 import { AiOutlineClose } from "react-icons/ai";
-import { Transition, Dialog } from "@headlessui/react";
+import { Transition } from "@headlessui/react";
 import logo from "../assets/logo.jpeg";
 import Image from "next/image";
 import { motion } from "framer-motion";
@@ -17,89 +17,69 @@ export default function Header() {
       viewport={{ once: true }}
       transition={{ ease: "easeInOut" }}
     >
-      <Transition show={open} appear as={Fragment}>
-        <Dialog
-          className="fixed inset-0 z-[9999] bg-white/0 backdrop-blur-md"
-          as={Fragment}
-          onClose={() => setOpen(false)}
-        >
-          <div className="min-h-screen">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
+      <Transition
+        show={open}
+        className="fixed inset-y-0 right-0 z-50 min-h-screen w-[100%] bg-white/0 backdrop-blur-md"
+        enter="transform duration-300"
+        enterFrom="translate-x-[100%]"
+        enterTo="translate-x-0"
+        leave="transform duration-300"
+        leaveFrom="translate-x-0"
+        leaveTo="translate-x-[100%]"
+      >
+        <div className="absolute right-0 min-h-screen w-[80%] bg-[#075264]">
+          <div className="flex w-full flex-col items-end px-4 pt-3">
+            <AiOutlineClose
+              fontSize={30}
+              className="text-white sm:hidden"
+              onClick={() => setOpen(!open)}
+            />
+            <div
+              className="group my-2 mt-12 flex w-full cursor-pointer items-start space-x-2 text-xl"
+              onClick={() => {
+                scrollTo("about");
+                setOpen(!open);
+              }}
             >
-              <Dialog.Overlay />
-            </Transition.Child>
-            <Transition.Child
-              as={Fragment}
-              enter="transform ease-in-out duration-300"
-              enterFrom="translate-x-[100%]"
-              enterTo="translate-x-0"
-              leave="transform ease-in-out duration-300"
-              leaveFrom="translate-x-0"
-              leaveTo="translate-x-[100%]"
+              <span className="text-muted">01.</span>
+              <span className="text-gray-100 underline-offset-2 group-hover:underline">
+                About
+              </span>
+            </div>
+            <div
+              className="group my-2 flex w-full cursor-pointer items-start space-x-2 text-xl"
+              onClick={() => {
+                scrollTo("work");
+                setOpen(!open);
+              }}
             >
-              <div className="absolute right-0 h-full w-[80%] bg-[#075264]">
-                <div className="flex w-full flex-col items-end px-4 pt-3">
-                  <AiOutlineClose
-                    fontSize={30}
-                    className="text-white sm:hidden"
-                    onClick={() => setOpen(!open)}
-                  />
-                  <div
-                    className="group my-2 mt-12 flex w-full cursor-pointer items-start space-x-2 text-xl"
-                    onClick={() => {
-                      scrollTo("about");
-                      setOpen(!open);
-                    }}
-                  >
-                    <span className="text-muted">01.</span>
-                    <span className="text-gray-100 underline-offset-2 group-hover:underline">
-                      About
-                    </span>
-                  </div>
-                  <div
-                    className="group my-2 flex w-full cursor-pointer items-start space-x-2 text-xl"
-                    onClick={() => {
-                      scrollTo("work");
-                      setOpen(!open);
-                    }}
-                  >
-                    <span className="text-muted">02.</span>
-                    <span className="text-gray-100 underline-offset-2 group-hover:underline">
-                      Work
-                    </span>
-                  </div>
-                  <div
-                    className="group my-2 flex w-full cursor-pointer items-start space-x-2 text-xl"
-                    onClick={() => {
-                      scrollTo("contact");
-                      setOpen(!open);
-                    }}
-                  >
-                    <span className="text-muted">03.</span>
-                    <span className="text-gray-100 underline-offset-2 group-hover:underline">
-                      Contact
-                    </span>
-                  </div>
-                  <a
-                    href="api/resume"
-                    download
-                    className="my-2 cursor-pointer self-start rounded bg-transparent 
+              <span className="text-muted">02.</span>
+              <span className="text-gray-100 underline-offset-2 group-hover:underline">
+                Work
+              </span>
+            </div>
+            <div
+              className="group my-2 flex w-full cursor-pointer items-start space-x-2 text-xl"
+              onClick={() => {
+                scrollTo("contact");
+                setOpen(!open);
+              }}
+            >
+              <span className="text-muted">03.</span>
+              <span className="text-gray-100 underline-offset-2 group-hover:underline">
+                Contact
+              </span>
+            </div>
+            <a
+              href="api/resume"
+              download
+              className="my-2 cursor-pointer self-start rounded bg-transparent 
         px-4 py-2 text-muted ring-1 ring-muted transition duration-200 hover:bg-muted hover:text-gray-100 sm:block"
-                  >
-                    Resume
-                  </a>
-                </div>
-              </div>
-            </Transition.Child>
+            >
+              Resume
+            </a>
           </div>
-        </Dialog>
+        </div>
       </Transition>
       <div className="mx-auto flex w-full max-w-7xl items-center justify-between bg-background/60 py-2 px-2 backdrop-blur-lg">
         <Image

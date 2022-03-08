e2bde6f8a380766cd98920c4778d0fb4968cedfd
Javaughn <javanpryce1@gmail.com>
2022-03-07 20:25:38 -0500

    commit

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="shortcut icon" href="public/favicon.ico" />
+        <link rel="shortcut icon" href="/static/logo.png" />
       </Head>
       <body>
         <Main />

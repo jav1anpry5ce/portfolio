f308db6cea74e763e7330fa276094a59e3c378f8
Javaughn Pryce <javanpryce1@gmail.com>
2022-04-27 07:53:05 -0500

    commit

--- pages/api/resume.js
+++ pages/api/resume.js
@@ -1,14 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import fs from "fs";
-import path from "path";
+import stream from "stream";
+import { promisify } from "util";
 
-const filePath = path.resolve(
-  __dirname,
-  "../../../public/Javaughn_Pryce_Resume.pdf"
-);
-const buffer = fs.readFileSync(filePath);
+const pipeline = promisify(stream.pipeline);
+const url =
+  "https://dl.dropbox.com/s/tcg5o2ya469bpmk/Javaughn_Pryce_Resume.pdf?dl=0";
 
 export default async function handler(req, res) {
+  const response = await fetch(url); // replace this with your API call & options
+  if (!response.ok)
+    throw new Error(`unexpected response ${response.statusText}`);
+
   res.setHeader("Content-Type", "application/pdf");
-  res.send(buffer);
+  res.setHeader(
+    "Content-Disposition",
+    "attachment; filename=Javaughn_Pryce_Resume.pdf"
+  );
+  await pipeline(response.body, res);
 }

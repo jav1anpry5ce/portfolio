2c7583160ac75cfcd78631efbd4db0c106d6043c
Javaughn Pryce <javanpryce1@gmail.com>
2022-04-29 09:34:37 -0500

    commit

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,10 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="shortcut icon" href="/static/logo.png" />
+        <meta
+          name="description"
+          content="Portfolio of Javaughn Pryce, a software engineer based in Kingston, Jamaica."
+        />
       </Head>
       <body>
         <Main />

21a2e2eb1ee89232aa89162c994121e63f6034a7
Javaughn <javanpryce1@gmail.com>
2022-06-08 18:10:52 -0400

    commit

--- pages/_document.js
+++ pages/_document.js
@@ -7,12 +7,7 @@ export default function Document() {
         <link rel="shortcut icon" href="/static/logo.png" />
         <meta
           name="description"
-          content="Portfolio of Javaughn Pryce, a software engineer based in Kingston, Jamaica."
-        />
-        <script
-          async
-          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE}`}
-          crossOrigin="anonymous"
+          content="Portfolio of Javaughn Pryce, a software engineer based in Philadelphia, PA."
         />
       </Head>
       <body>

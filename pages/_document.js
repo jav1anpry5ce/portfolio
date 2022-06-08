345bf17897839289d27e0f1c2a2eda8b17182ddc
Javaughn <javanpryce1@gmail.com>
2022-06-08 18:34:33 -0400

    commit

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,10 @@ export default function Document() {
           name="description"
           content="Portfolio of Javaughn Pryce, a software engineer based in Philadelphia, PA."
         />
+        <meta
+          name="google-site-verification"
+          content="Jvl6IV3Hsp4Q2LKMU-RPOJKuSCeyc270CKS5p5uRqUw"
+        />
       </Head>
       <body>
         <Main />

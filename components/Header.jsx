9240cef1386c9a349e7c647796626e211822706e
Javaughn <javanpryce1@gmail.com>
2024-02-17 13:06:59 -0500

    new logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -83,7 +83,7 @@ export default function Header() {
         <div className="mx-auto flex w-full max-w-7xl items-center justify-between p-2">
           <a href="#main" onClick={() => scrollTo(mainRef)} tabIndex="1">
             <Image
-              src="/images/logo.jpeg"
+              src="/images/head_shot.jpg"
               alt="logo"
               width={55}
               height={55}

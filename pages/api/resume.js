cd86948f5f6b2851edb76a59a7477c1adf23cb35
Javaughn <javanpryce1@gmail.com>
2024-06-13 08:42:55 -0400

    added new resume

--- pages/api/resume.js
+++ pages/api/resume.js
@@ -4,7 +4,7 @@ import { promisify } from "util";
 
 const pipeline = promisify(stream.pipeline);
 const url =
-  "https://dl.dropbox.com/scl/fi/dybeqhnplpc7ox1nrvjuu/Javaughn_Pryce_Resume.pdf?rlkey=ve4wiz4y6y721gb3mx2s7fmot&st=rv6crzcn&dl=0";
+  "https://dl.dropbox.com/scl/fi/n8j03pkowpp95tkjfouao/Javaughn_Pryce_Resume.pdf?rlkey=9df1vcnxax37gg1vxz0nph6l0&st=zf92taye&dl=0";
 
 export default async function handler(req, res) {
   const response = await fetch(url); // replace this with your API call & options
